267eb0edacf19735ad701b07944344a2beeb8b35
Marc A. Modrow <modrow@traum-ferienwohnungen.de>
2019-04-03 17:27:34 +0200

    make clock use colormap colours instead of brightness alone

--- LuxaforSocketListener.py
+++ LuxaforSocketListener.py
@@ -54,35 +54,14 @@ def setColorByLabel(label):
         sleep(3)
         flagstatus = state
     elif hexColor.match(label):
-        match = re.match(hexColor, label)
-        matchGroups = match.groups()
-        hexRed = int(matchGroups[0], 16)
-        hexGreen = int(matchGroups[1], 16)
-        hexYellow = int(matchGroups[2], 16)
-        flagstatus = [[LuxaforFlag.LED_ALL, hexRed, hexGreen, hexYellow]]
+        newColor = getColorFromHexString(label)
+        flagstatus = [getFlagColor(LuxaforFlag.LED_ALL, newColor)]
     elif time.match(label):
-        match = re.match(time, label)
-        matchGroups = match.groups()
-        hours = int(matchGroups[0])
-        hoursOfDay = max(9, min(18, hours)) - 9
-        hoursBrightness = int((9.0-hoursOfDay)/9*255)
-        minutes = int(matchGroups[1])
-        minutesOfHour = max(0, min(60, minutes))
-        minutesBrightness = int((59.0-minutesOfHour)/60*255)
-        seconds = int(matchGroups[2])
+        newColors = getColorsFromTimeString(label)
 
-        hoursLed = [
-                        LuxaforFlag.LED_BACK_2,
-                        hoursBrightness,
-                        hoursBrightness,
-                        hoursBrightness
-                    ]
-        minutesLed = [
-                        LuxaforFlag.LED_BACK_1,
-                        minutesBrightness,
-                        minutesBrightness,
-                        minutesBrightness
-                    ]
+        hoursLed = getFlagColor(LuxaforFlag.LED_BACK_2, newColors[0])
+        minutesLed = getFlagColor(LuxaforFlag.LED_BACK_1,newColors[1])
+
         flagstatus = [hoursLed, minutesLed]
     setColor(flagstatus)
 
@@ -91,6 +70,71 @@ def setColor(flagstatus):
     state = flagstatus
     for ledSet in flagstatus :
         flag.do_static_colour(leds=ledSet[0], r=ledSet[1], g=ledSet[2], b=ledSet[3])
+
+
+#-------------------------------------------------------------------------
+#                              Color Handling                            #
+#-------------------------------------------------------------------------
+def getFlagColor(flag, color):
+    return [flag, color[0], color[1], color[2]]
+
+def getColorFromHexString(hexString):
+    match = re.match(hexColor, hexString)
+    matchGroups = match.groups()
+    hexRed = int(matchGroups[0], 16)
+    hexGreen = int(matchGroups[1], 16)
+    hexYellow = int(matchGroups[2], 16)
+    return [hexRed, hexGreen, hexYellow]
+
+def getColorsFromTimeString(timeString):
+    match = re.match(time, timeString)
+    matchGroups = match.groups()
+
+    hours = int(matchGroups[0])
+    hoursOfDay = max(9, min(18, hours)) - 9
+    hoursPartial = hoursOfDay / 9.0
+    hoursColor = get7StopHeatMapColor(hoursPartial)
+
+    minutes = int(matchGroups[1])
+    minutesOfHour = max(0, min(59, minutes))
+    minutesPartial = minutesOfHour / 59.0
+    minutesColor = get7StopHeatMapColor(minutesPartial)
+
+    seconds = int(matchGroups[2])
+    secondsOfMinute = max(0, min(59, seconds))
+    secondsPartial = secondsOfMinute / 59.0
+    secondsColor = get7StopHeatMapColor(secondsPartial)
+
+    return [hoursColor, minutesColor, secondsColor]
+
+def get7StopHeatMapColor(value):
+    stops = [
+        [000, 000, 000], # [0] -   0,00% : Black
+        [000, 000, 255], # [1] -  16,66% : Blue
+        [000, 255, 255], # [2] -  33,33% : Cyan
+        [000, 255, 000], # [3] -  50,00% : Green
+        [255, 255, 000], # [4] -  66,66% : Yellow
+        [255, 000, 000], # [5] -  83,33% : Red
+        [255, 255, 255], # [6] - 100,00% : White
+    ]
+
+    numStops = len(stops)
+    steps = numStops - 1
+    stepWidth = 1.0 / steps
+
+    below = max(int(steps*value), 0)
+    above = min(below + 1, steps)
+
+    stepProgress = (value - float(below) * stepWidth) / stepWidth
+
+    return getValueBetweenTwoColors(stepProgress, stops[below], stops[above])
+
+def getValueBetweenTwoColors(value, color1, color2):
+    newRed   = int(float(color2[0] - color1[0]) * value + color1[0])
+    newGreen = int(float(color2[1] - color1[1]) * value + color1[1])
+    newBlue  = int(float(color2[2] - color1[2]) * value + color1[2])
+
+    return [newRed, newGreen, newBlue]
 
 #-------------------------------------------------------------------------
 #                             Socket Listener                            #
