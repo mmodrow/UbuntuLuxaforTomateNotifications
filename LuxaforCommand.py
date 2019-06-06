25e7a3bef771ad8ccc2df09263231217072e1fa2
Marc A. Modrow <modrow@traum-ferienwohnungen.de>
2019-06-06 14:34:48 +0200

    add help parameter to command script

--- LuxaforCommand.py
+++ LuxaforCommand.py
@@ -13,6 +13,20 @@ def get_script_dir(follow_symlinks=True):
         path = os.path.realpath(path)
     return os.path.dirname(path)
 
+def printHelp():
+    message = "The following commands are allowed:\n"\
+        + "   e.g. \"14:28:01\" -- display a time as day/hour progress heatmap\n"\
+        + "      (going through black, blue, cyan, green, yellow, red, white)\n"\
+        + "   e.g. \"#000000\" -- any hex colour\n"\
+        + "   e.g. lime -- any hex colour's css name\n\n"\
+        + "   following animation pattern names:\n"\
+        + "      confused -- wild flickering\n"\
+        + "      extra_confused -- even wilder flickering\n"\
+        + "      cops -- red/blue siren\n"\
+        + "      rainbow -- cycle through the rainbow\n"\
+        + "      luxafor -- a default showcase pattern"
+    print(message)
+
 #append the relative location you want to import from
 os.chdir(get_script_dir())
 #print(get_script_dir())
@@ -38,6 +52,11 @@ try:
     
     # Send data
     message = sys.argv[1]
+
+    if sys.argv[1] == "help":
+        printHelp()
+        sys.exit()
+
     try:
         message = webcolors.name_to_hex(message)
         print(sys.argv[1] + " is name for hex color. Using hex code " + message + " instead")
